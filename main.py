61a642e75f7e02bb9ec8d67dcc785e111bb378b1
IGR2020 <159914009+IGR2020@users.noreply.github.com>
2024-11-22 16:00:48 +0530

    Image Viewing

--- main.py
+++ main.py
@@ -1,37 +1,63 @@
 import sys
 from PySide6 import QtGui, QtCore, QtWidgets
 from PySide6.QtCore import Signal
+from PySide6.QtWidgets import *
 
 windowWidth, windowHeight = 900, 500
 
-class WelcomeScreen(QtWidgets.QWidget):
+class Image(QtWidgets.QLabel):
+    def __init__(self, path: str, scale: int = 1, **kwargs):
+        super().__init__(**kwargs)
+
+        pixelMap = QtGui.QImage(path)
+        imageWidth, imageHeight = pixelMap.width(), pixelMap.height()
+        pixelMap = pixelMap.scaled(imageWidth*scale, imageHeight*scale)
+        self.setPixmap(QtGui.QPixmap.fromImage(pixelMap))
+
+class App(QtWidgets.QStackedWidget):
     def __init__(self):
         super().__init__()
+        self.addWidget(WelcomeScreen(self))
+        self.addWidget(Editor(self))
 
-        self.greetings = ["IGR2020", "MOG2129", "AAO2009"]
-        self.currentGreeting = 0
+        self.setCurrentIndex(0)
+
+class WelcomeScreen(QtWidgets.QWidget):
+    def __init__(self, parentApp: QtWidgets.QStackedWidget):
+        super().__init__()
+        self.parentApp = parentApp
 
         self.button = QtWidgets.QPushButton("Start Wipy")
-        self.text = QtWidgets.QLabel("Hello IGR2020", alignment=QtCore.Qt.AlignCenter)
+        self.text = QtWidgets.QLabel("Welcome To Wipy", alignment=QtCore.Qt.AlignCenter)
 
         self.button.clicked.connect(self.start)
 
         self.layout = QtWidgets.QVBoxLayout(self)
-
 
         self.layout.addWidget(self.text)
         self.layout.addWidget(self.button)
 
     @QtCore.Slot()
     def start(self):
-        self.close()
+        self.parentApp.setCurrentIndex(1)
 
+
+class Editor(QtWidgets.QWidget):
+    def __init__(self, parentApp: QtWidgets.QStackedWidget):
+        super().__init__()
+        self.parentApp = parentApp
+
+        self.image = Image("Texture.png", 10, alignment=QtCore.Qt.AlignCenter)
+
+        self.layout = QVBoxLayout(self)
+
+        self.layout.addWidget(self.image)
 
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
 
-    widget = WelcomeScreen()
+    widget = App()
     widget.resize(windowWidth, windowHeight)
     widget.show()
 
